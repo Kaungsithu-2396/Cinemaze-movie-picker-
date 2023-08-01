434de12c8303b086b6627a9aecca95ae5294af74
Kaungsithu-123 <Kaungsithu2396@gmail.com>
2023-08-01 20:17:35 +0630

    component for each section of movies done

--- app/components/Movies.tsx
+++ app/components/Movies.tsx
@@ -4,20 +4,35 @@ type props = {
 };
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 export default function Movies({ movies }: props) {
-    console.log(movies);
     return (
-        <div className="h-screen m-10">
-            <h1 className="text-3xl my-5 text-amber-400 font-extrabold">
+        <div className=" h-screen m-10">
+            <h1 className="text-4xl my-10 text-amber-400 font-extrabold">
                 Upcoming <span className="text-white">movies</span>
             </h1>
             <Splide
                 options={{
-                    arrows: false,
+                    arrows: true,
                     mediaQuery: "max",
+                    pagination: false,
+                    gap: 10,
+                    keyboard: "global",
+                    type: "loop",
                     perPage: 3,
+                    autoplay: true,
+                    pauseOnHover: true,
                     breakpoints: {
-                        640: {
-                            perPage: 1,
+                        480: {
+                            arrows: true,
+                            gap: 20,
+                            pagination: false,
+                            // arrows: false,
+                            // pagination: true,
+                            perPage: 2,
+                        },
+                        768: {
+                            arrows: false,
+                            gap: 30,
+                            perPage: 3,
                         },
                     },
                 }}
@@ -25,16 +40,27 @@ export default function Movies({ movies }: props) {
                 {movies.results.map((movie) => {
                     return (
                         <SplideSlide key={movie.id}>
-                            <div className="relative cursor-pointer">
+                            <div className="relative  cursor-pointer">
                                 <img
                                     src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}
                                     `}
                                     alt=""
-                                    className="w-full h-full"
+                                    className="w-full h-[20rem] object-cover rounded-md"
                                 />
                                 <div
-                                    className={`absolute  hover:bg-black/60 duration-200 delay-200 w-full h-full  top-0`}
-                                ></div>
+                                    className={`absolute hover:bg-black/60 bg-black/20 duration-200 delay-200 w-full h-full  top-0`}
+                                >
+                                    <h1 className="text-center invisible lg:visible  px-4 h-full flex items-center justify-center text-xl opacity-0 font-extrabold text-amber-400 hover:opacity-100 duration-300 delay-300">
+                                        {movie.title}
+                                        {movie.title !== movie.original_title &&
+                                            "(" + movie.original_title + ")"}
+                                    </h1>
+                                </div>
+                            </div>
+                            <div className="lg:hidden block my-4">
+                                <h1 className="text-center md:text-xl lg:text-2xl text-sm">
+                                    {movie.title}
+                                </h1>
                             </div>
                         </SplideSlide>
                     );
